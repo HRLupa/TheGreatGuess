adab9a16eed88d0e7cdfbfed47d1accaeb2d8f84
Lupa <hrlupa0@gmail.com>
2025-06-24 21:21:30 +0200

    Moving the static values to another file

--- Test connaissances vidéos tgr.py
+++ Test connaissances vidéos tgr.py
@@ -2,6 +2,7 @@ import json
 import random
 from datetime import timedelta
 from unidecode import unidecode
+from statiques import francais_anglais,manual_aliases
 
 # Convertit secondes -> HH:MM:SS
 def seconds_to_hms(seconds):
@@ -131,66 +132,6 @@ def quiz(transcripts, title_map, n=5):
             guess_time+=1 #Don't know why but there's a missing second if I don't add this one
         print(f"Vous étiez à {seconds_to_hms(abs(start_time-guess_time))} du temps réel, c'était à {seconds_to_hms(start_time)}")
     print(f"\n🎉  Score final : {score}/{n}")
-
-# Exemple d'alias personnalisés à ajouter
-francais_anglais={'Mais pourquoi Nintendo porte plainte contre tout le monde ?': 'Mais pourquoi Nintendo porte plainte contre tout le monde ?', 'Quand 500 jeunes ont tenu une montagne contre les nazis': 'The time 500 youngsters held a mountain against the Nazis', 'Le jeu où on plante des sapins': 'Planting trees and feeling happy', "22 minutes de + pour comprendre l'univers": 'Another 22 minutes to understand the universe', 'La légende de Barbe Scintillante': 'The legend of Glitterbeard', "Créer (et détruire) la plus grosse licence d'Occident": 'How to build (and destroy) the largest franchise in the west', 'La plus belle des équipes': 'The most beautiful team there was', "22 minutes pour sauver l'univers (ok un peu plus)": '22 minutes to save the universe (alright, maybe a bit more)', "Qu'est-ce que le cinéma a appris au jeu vidéo ?": 'What video games learnt from cinema', "C'est quoi un bon film ?": 'What\'s a "good" movie ?', 'Tunic : Le jeu qui en cachait un autre': 'The game that was hiding another', 'Le mystère du 8': '88888888', 'Les clubs du futur': 'Gaming teams of the future', 'La géopolitique expliquée avec des pixels': 'Geopolitics explained through pixels', "La plus grande partie de capture de drapeau de l'histoire d'Internet": 'Greatest game of capture the flag ever played', 'Comment Among Us a explosé': 'How Among Us made it', 'La quête du dernier secret': 'The last big Secret', "L'incroyable histoire d'Otzdarva et de la run Dark Souls la plus difficile": 'The incredible story of Otzdarva and the hardest Dark Souls run'}
-manual_aliases = {
-    "Mais pourquoi Nintendo porte plainte contre tout le monde ?": [
-        "nintendo","plaintes"
-    ],
-    "Quand 500 jeunes ont tenu une montagne contre les nazis": [
-        "jeunes", "montagne", "nazis", "résistance", "glières"
-    ],
-    "Le jeu où on plante des sapins": [
-        "sapins", "Terra Nil", "jeu sapins"
-    ],
-    "22 minutes de + pour comprendre l'univers": [
-        "outer wilds 2","outer wilds dlc","ow2","hibous","ow 2","+"
-    ],
-    "La légende de Barbe Scintillante": [
-        "barbe scintillante", "barbe", "sea of thieves","sot"
-    ],
-    "Créer (et détruire) la plus grosse licence d'Occident": [
-        "créer licence","occident","cod","activision","infinity ward","infinityward"
-    ],
-    "La plus belle des équipes": [
-        "équipe", "lol","rox tigers","roxtigers","rox","équipes","belle"
-    ],
-    "22 minutes pour sauver l'univers (ok un peu plus)": [
-        "22 minutes", "outer wilds 1","outer wilds","ow1","ow 1","ow"
-    ],
-    "Qu'est-ce que le cinéma a appris au jeu vidéo ?": [
-        "jeu vidéo","cinéma jeu vidéo","cinéma vs jeu vidéo"
-    ],
-    "C'est quoi un bon film ?": [
-        "bon film", "film","cinéma","meilleurs"
-    ],
-    "Tunic : Le jeu qui en cachait un autre": [
-        "tunic", "caché", "renard","ronard"
-    ],
-    "Le mystère du 8": [
-        "le mystère du 8", "Stanley", "The Stanley Parable", "88888888", "8","trafiquer"
-    ],
-    "Les clubs du futur": [
-        "clubs", "clubs futur", "futur", "clubs du futur","futurs","villes"
-    ],
-    "La géopolitique expliquée avec des pixels": [
-        "place", "pixels", "rplace", "r/place","pixel war","reddit","guerre"
-    ],
-    "La plus grande partie de capture de drapeau de l'histoire d'Internet": [
-        "capture de drapeau", "capture drapeau", "ctf", "internet","pol","4chan","histoire","shia","laboeuf","shia laboeuf","grenouille","commando"
-    ],
-    "Comment Among Us a explosé": [
-        "among us", "explosé", "succès among us","among us succès"
-    ],
-    "La quête du dernier secret": [
-        "dernier secret", "quête", "quête dernier", "secret","quête dernier secret","Shadow of the Colossus","sotc","porte"
-    ],
-    "L'incroyable histoire d'Otzdarva et de la run Dark Souls la plus difficile": [
-        "otzdarva", "dark souls", "run","histoire otzdarva","ds","dark souls 2","ds2","abnh","all bosses no hit"
-    ]
-}
-#print({list(manual_aliases.keys())[i]:list(get_durations().keys())[i] for i in range(len(manual_aliases))})
 
 if __name__ == "__main__":
     transcripts = load_transcripts()
