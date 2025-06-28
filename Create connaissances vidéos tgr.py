50a4aa3a25d3a6fd297b02e00395a4aaff0d68f1
Lupa <hrlupa0@gmail.com>
2025-06-28 11:43:46 +0200

    Updated the creation so it's more automatic now

--- Create connaissances vidéos tgr.py
+++ Create connaissances vidéos tgr.py
@@ -1,20 +1,25 @@
 import json
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound, VideoUnavailable
 
+def get_channel(name):
+    import subprocess
+    subprocess.run(f'yt-dlp --flat-playlist --dump-single-json "https://www.youtube.com/@{name}"',stdout=open("videos.json","w"))
+    from pathlib import Path
+    path = Path("videos.json")
+    path.write_text(path.read_text(), encoding="utf8")
 def load_all_videos_from_channel_json(json_file):
     with open(json_file, encoding="utf-8") as f:
         data = json.load(f)
 
     all_videos = []
-    playlists = data.get("entries", [])
-    for playlist in playlists:
-        playlist_entries = playlist.get("entries", [])
-        for video in playlist_entries:
-            if video.get("_type") == "url" and video.get("ie_key") == "Youtube" and video.get("id"):
-                all_videos.append({
-                    "id": video["id"],
-                    "title": video.get("title", "Titre inconnu")
-                })
+    playlist = data.get("entries", [])[0]
+    playlist_entries = playlist.get("entries", [])
+    for video in playlist_entries:
+        if video.get("_type") == "url" and video.get("ie_key") == "Youtube" and video.get("id"):
+            all_videos.append({
+                "id": video["id"],
+                "title": video.get("title", "Titre inconnu")
+            })
     return all_videos
 
 def get_transcripts(video_list):
@@ -35,6 +40,7 @@ def save_transcripts(transcripts, filename="transcripts.json"):
         json.dump(transcripts, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
+    get_channel("TheGreatReview")
     videos = load_all_videos_from_channel_json("videos.json")
     print(f"{len(videos)} vidéos extraites.")
 
